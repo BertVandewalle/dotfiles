61cd591f4b30d4295caa6259bcf30860aaa46807
Bert Vandewalle <bert.vandewalle@cnhind.com>
2025-08-28 22:05:27 +0200

    Initial commit of project files

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,40 @@
+require("core.options")
+require("core.keymaps")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+print(lazypath)
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	print("cloning lazyrepo")
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
+end
+
+local rtp = vim.opt.rtp
+rtp:prepend(lazypath)
+
+if vim.g.vscode then
+	require("lazy").setup({
+		require("plugins.treesitter"),
+		require("plugins.telescope"),
+	})
+else
+	require("lazy").setup({
+		require("plugins.neo-tree"),
+		require("plugins.colortheme"),
+		require("plugins.bufferline"),
+		require("plugins.lualine"),
+		require("plugins.treesitter"),
+		require("plugins.telescope"),
+		require("plugins.lsp"),
+		require("plugins.autocompletion"),
+		require("plugins.none-ls"),
+		require("plugins.gitsigns"),
+		require("plugins.alpha"),
+		require("plugins.indent-blankline"),
+		require("plugins.misc"),
+		require("plugins.debug"),
+	})
+end
